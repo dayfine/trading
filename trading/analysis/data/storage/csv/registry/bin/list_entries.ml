4773f33997a6b5f24ace9a4f1af59a678fb04c78
Docker <docker@example.com>
2025-06-21 08:58:18 +0100

    wip - iterate over the registry

--- trading/analysis/data/storage/csv/registry/bin/list_entries.ml
+++ trading/analysis/data/storage/csv/registry/bin/list_entries.ml
@@ -1,11 +1,38 @@
 open Core
 open Registry
 
+(* Save the metadata to a file next to the CSV file *)
+let metadata_path_for_csv csv_path =
+  let path_string = Fpath.to_string csv_path in
+  Fpath.v (String.chop_suffix_exn path_string ~suffix:".csv" ^ ".metadata.sexp")
+
+let open_metadata_file ~csv_path =
+  File_sexp.Sexp.load (module Metadata.T_sexp)
+    ~path:(metadata_path_for_csv csv_path)
+
+let wanted metadata =
+  let open Metadata in
+  metadata.has_volume
+  && not metadata.last_n_prices_avg_below_10
+  && not metadata.last_n_prices_avg_above_500
+
 let list_n_entries t n =
   let entries = list t in
   let entries = List.take entries n in
-  List.iter entries ~f:(fun { symbol; csv_path } ->
-      printf "%s: %s\n" symbol (Fpath.to_string csv_path))
+  let total = List.length entries in
+  let total =
+    List.fold entries ~init:0 ~f:(fun acc { symbol; csv_path } ->
+      match open_metadata_file ~csv_path with
+      | Ok metadata ->
+          let wanted = wanted metadata in
+          if wanted then
+            acc + 1
+          else
+            acc
+      | Error _ -> acc
+    )
+  in
+  printf "Total wanted: %d\n" total
 
 let show_entry t symbol =
   match get t ~symbol with
